7c4b1bf2df5c0b7532e2734b64159a093fbab6ad
elkinQuintero <santi-1375@hotmail.com>
2024-01-23 16:48:19 -0500

    feat: Add cart project

---
+++ 5-cart-app/src/data/productos.js
@@ -0,0 +1,38 @@
+export const products = [
+    {
+        id: 1,
+        name: 'Teclado Mecanico RGB',
+        description: 'Teclado mecanico con luces RGB y switches cherry red',
+        price: 1000
+    },
+    {
+        id: 2,
+        name: 'Samsung Smart TV 55',
+        description: 'Excelente tv LCD',
+        price: 3000
+    },
+    {
+        id: 3,
+        name: 'Audifonos Bluetooth Sony',
+        description: 'Audifonos bluetooth para smartphone',
+        price: 760
+    },
+    {
+        id: 4,
+        name: 'Memoria Corsair 8GB DDR5',
+        description: 'Memoria RAM optimizada para juegos',
+        price: 3000
+    },
+    {
+        id: 5,
+        name: 'Asus NVIDIA RTX',
+        description: 'Tarjeta gráfica para juego en 8k',
+        price: 5000
+    },
+    {
+        id: 6,
+        name: 'CPU Intel Core i7',
+        description: 'Optimizada para tareas multicore',
+        price: 4000
+    }
+]
